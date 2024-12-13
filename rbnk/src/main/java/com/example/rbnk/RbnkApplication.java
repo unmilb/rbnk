b5a82a424d1bd9cfbdc07047fcb7e9bc300bb6a3
unmilb <unmilbind@gmail.com>
2024-12-13 15:09:01 +0530

    Init

---
+++ rbnk/src/main/java/com/example/rbnk/RbnkApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbnkApplication.class, args);
+	}
+
+}
---
+++ rbnk/src/test/java/com/example/rbnk/RbnkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
